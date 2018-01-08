59f29b3f89938993491e8b1ab41b1b99a86e410d
Alivelu Ravula <alivelu7193@achiever-25.local>
2018-01-08 11:17:03 +0530

    Initial Commit

---
+++ IndiaArray/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  IndiaArray
+//
+//  Created by Alivelu Ravula on 1/8/18.
+//  Copyright © 2018 Alivelu Ravula. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
